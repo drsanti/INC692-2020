738613be2f8543f2932f69236ea86ca24d24a7bc
drsanti <santi.inc.kmutt@gmail.com>
2020-01-30 12:11:50 +0700

    update

---
+++ Embedded/README.md
@@ -0,0 +1,4 @@
+## Links:
+```
+- https://www.freertos.org/
+```
--- Embedded/main.c
+++ Embedded/main.c
@@ -1,34 +1,35 @@
 
 #include "ecc_pic24_bps.h"
 
-void Task1(void *pvParameters)
-{
-	for (;;)
-	{
-		LED0_LAT = !LED0_LAT;
-		vTaskDelay(50 / portTICK_PERIOD_MS);
-	}
+// Task Code
+void vTask_LED0(void* pvParameters) {
+    for (;;) {
+        LED0_LAT = !LED0_LAT;                   // Toggle LED3
+        vTaskDelay(8000 / portTICK_PERIOD_MS);  // Wait for .1 seconds
+    }
+}
+void vTask_LED3(void* pvParameters) {
+    while (1) {
+        LED3_LAT = !LED3_LAT;                                     // Toggle LED0
+        vTaskDelay((uint32_t)pvParameters / portTICK_PERIOD_MS);  // Wait for T seconds
+    }
 }
 
-void Task2(void *pvParameters)
-{
-	for (;;)
-	{
-		LED1_LAT = !LED1_LAT;
-		vTaskDelay(300 / portTICK_PERIOD_MS);
-	}
-}
+int main(void) {
+    // Hardware Setup
+    System_Init();
 
-int main(void)
-{
-	System_Init();
+    // Turn OFF LED<3:0>
+    LED0_LAT = LED1_LAT = 1;
+    LED2_LAT = LED3_LAT = 1;
 
-	LED2_LAT = LED3_LAT = 1;
+    // Create Tasks
+    xTaskCreate(vTask_LED0, NULL, 128, NULL, 0, NULL);
+    xTaskCreate(vTask_LED3, NULL, 128, (void*)100, 0, NULL);
 
-	xTaskCreate(Task1, NULL, 128, NULL, tskIDLE_PRIORITY, NULL);
-	xTaskCreate(Task2, NULL, 128, NULL, tskIDLE_PRIORITY, NULL);
+    // Start
+    vTaskStartScheduler();
 
-	vTaskStartScheduler();
-
-	return 0;
+    //
+    return 0;
 }
