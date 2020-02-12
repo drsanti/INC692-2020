05b51a8b87c766a8b5589120eb4d68eaeb4ecd50
drsanti <santi.inc.kmutt@gmail.cm>
2020-02-12 19:44:25 +0700

    update

--- Examples/ex01_create_task/main.c
+++ Examples/ex01_create_task/main.c
@@ -18,8 +18,8 @@ void vTask_LED3(void *pvParameters)
     char buff[32];
     while (1)
     {
-        sprintf(buff, "counter: %d\r\n", counter++);
-        UART1_Write(buff);
+        //sprintf(buff, "counter: %d\r\n", counter++);
+        //UART1_Write(buff);
 
         LED3_LAT = !LED3_LAT; // Toggle LED3
         vTaskDelay((*ticks) / portTICK_PERIOD_MS);
@@ -36,9 +36,9 @@ int main(void)
     LED2_LAT = LED3_LAT = 1;
 
     // Create Tasks
-    xTaskCreate(vTask_LED0, NULL, 128, (void *)500, 0, NULL); // pass a constant value as the parameter
+    xTaskCreate(vTask_LED0, NULL, 128, (void *)200, 0, NULL); // pass a constant value as the parameter
 
-    TickType_t ticks = 200;
+    TickType_t ticks = 20;
     xTaskCreate(vTask_LED3, NULL, 128, (void *)&ticks, 0, NULL); // pass an address of variable as the parameter
 
     // Start
