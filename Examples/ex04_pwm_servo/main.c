5247e65ca1a5ec13048444f5f5c3707f7da14364
jjjadey <nattakan.premp@gmail.com>
2020-02-13 12:27:53 +0700

    update

--- Examples/ex04_pwm_servo/main.c
+++ Examples/ex04_pwm_servo/main.c
@@ -3,6 +3,45 @@
 #include "ecc_pic24_bsp.h"
 
 
+/**
+ * Hardware connection for PWMs
+ * - PWM_A --> LED2
+ * - PWM_B --> LED3  
+ * - PWM_C --> SCL  
+ * - PWM_D --> SDA  
+ */
+
+
+// CUP Clock:	32 MHz
+// TMR Clock:	32MZ/2 -> 16MHz/64 -> 250 kHz --> 4 uS;
+
+/*
+ 250 kHz   -----> TMR2 -------> OC
+ ( 4uS )          PR2=xx       OC1RS=??
+                 (20mS)        (1.5mS)
+
+*/
+//                    (250 kHz) /     (50) = 5000 -> 20 mS
+#define __PR2 		((32e6/2)/64) / (1/20e-3)
+
+//                     (250 kHz)  /  (1000) 	// 250
+#define __OC_MIN	((32e6/2)/64) / (1/1e-3)	// 1.0mS ->   0dg
+
+
+//                     (250 kHz)  /  (~667) 	// 375
+#define __OC_MID	((32e6/2)/64) / (1/1.5e-3)	// 105mS ->   90dg
+
+//                     (250 kHz)  /  (500) 		// 500
+#define __OC_MAX	((32e6/2)/64) / (1/2e-3)	// 2.0mS ->  180dg
+
+
+
+// User input (deg 0->180) [ ? ] -> OCxRS
+// 0   --> [?] --> OCxRS = 250
+// 90  --> [?] --> OCxRS = 375
+// 180 --> [?] --> OCxRS = 500
+
+
 uint16_t __dt =  2;
 uint16_t __sf =  40;
 uint16_t __pr =  (16e6/64)/(1.0/20e-3);
@@ -12,29 +51,26 @@ void PWM_Init(void)
 	//*******************************************
 	//* Setup Timer2 for OC Clock source        *
 	//*******************************************
-	//T2CONbits.TCKPS	 	= 0;		// 1:1 pre-scaler
-	T2CONbits.TCKPS		= 2;		// 1:64 prescaller
-	T2CONbits.T32	 	= 0;		// 16-bit Timer
-	T2CONbits.TCS	 	= 0;		// Internal clock (FOSC/2)
-	T2CONbits.TGATE	 	= 0;		// Gated time accumulation disabled
-	T2CONbits.TON		= 1;		// Start Timer
-	PR2					= __pr;		// Timer Period register, 100kHz, @FCY=16MHz(FOSC=32MHz)
-	IEC0bits.T2IE		= 0;		// Enable Timer2 Interrupt
+
+	T2CONbits.TCKPS		= 2;			// 0->1:1, 1->1:8, 2->1:64, 3->1:256 prescaller
+	T2CONbits.T32	 	= 0;			// 16-bit Timer
+	T2CONbits.TCS	 	= 0;			// Internal clock (FOSC/2)
+	T2CONbits.TGATE	 	= 0;			// Gated time accumulation disabled
+	T2CONbits.TON		= 1;			// Start Timer
+	PR2					= __PR2;		// Timer Period register, for 20 mS 
+	IEC0bits.T2IE		= 0;			// Enable Timer2 Interrupt
 
 
-	//---------OC1 [A] ------------------------------------------------------
-	OC1R 				= 1;		// Hi
-	//OC1RS 			= __pr>>1;	// Lo
-	OC1RS 				= __pr*(99.0/100.0);
-	OC1CONbits.OCM		= 5;		// Dual Compare, Continous pulse output
-	OC1CONbits.OCTSEL 	= 0;		// Timer2 is the clock source for Output Compare x
+	//---------OC1 [A, PWM_A, LED2] ------------------------------------------------------
+	OC1R 				= 0;				// Go High (1)
+	OC1RS 				= __OC_MIN;			// Go low (0) --> , for 1.5 mS (90 dg)
+	OC1CONbits.OCM		= 5;			// Dual Compare, Continous pulse output
+	OC1CONbits.OCTSEL 	= 0;			// Timer2 is the clock source for Output Compare x
 
 
-	//---------OC2 [B] ------------------------------------------------------
-	// OC2R 			= OC1RS + __dt + 1 ;
-	// OC2RS 			= __pr   - __dt;
-	OC2R 				= 1;
-	OC2RS 				= __pr*(5.0/100.0);
+	//---------OC2 [B, PWM_B, LED3] ------------------------------------------------------
+	OC2R 				= 0;
+	OC2RS 				= __OC_MAX;
 	OC2CONbits.OCM		= 5;
 	OC2CONbits.OCTSEL 	= 0;
 
